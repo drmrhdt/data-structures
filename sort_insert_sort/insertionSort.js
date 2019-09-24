e5b4d94db9b98091b880d572813263f40bf20cbd
Popova Daria <drmrhdt@mail.ru>
2019-09-24 16:16:07 +0300

    added implementing_array and sort_insert_sort (no time measurement)

---
+++ sort_insert_sort/insertionSort.js
@@ -0,0 +1,28 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(arrayToSort) {
+  let sortedArray = arrayToSort;
+  
+  // check every element in unsorted array
+  for (let currentIndex = 1; currentIndex < sortedArray.length; currentIndex++) {
+      
+      // choose cur element (start with array[1]) and compare it with subarray until that element
+      for (let currentIndexToCompare = currentIndex; currentIndexToCompare > 0; currentIndexToCompare--) {
+
+        // compare cur element with previous element
+        if (sortedArray[currentIndexToCompare] < sortedArray[currentIndexToCompare - 1]) {
+
+          // if cur > prev --> swap them
+          let currentValue = sortedArray[currentIndexToCompare];
+          sortedArray[currentIndexToCompare] = sortedArray[currentIndexToCompare - 1];
+          sortedArray[currentIndexToCompare - 1] = currentValue;
+          }
+      }
+
+      console.log(`curEl: ${currentIndex} sorted array: ${sortedArray}`);
+  }
+  return sortedArray;
+}
+
+insertionSort(numbers);
+console.log(numbers);
